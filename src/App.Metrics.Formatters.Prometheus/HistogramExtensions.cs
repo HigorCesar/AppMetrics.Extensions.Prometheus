e2d10a4cb705f28b0adcc1998330ce7a4c722e3c
Higor Ramos <higor.crr@gmail.com>
2017-05-22 10:18:03 +0200

    #3 Histogram time value to seconds

---
+++ src/App.Metrics.Formatters.Prometheus/HistogramExtensions.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="HistogramExtensions.cs" company="Allan Hardy">
+// Copyright (c) Allan Hardy. All rights reserved.
+// </copyright>
+using System;
+
+namespace App.Metrics.Formatters.Prometheus
+{
+    public static class HistogramExtensions
+    {
+        public static double ToSeconds(this double value) => TimeSpan.FromMilliseconds(value).TotalSeconds;
+    }
+}
